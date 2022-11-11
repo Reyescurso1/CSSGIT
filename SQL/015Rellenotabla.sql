c800c6f0331b80340255b9c749ec91d78e3ad15f
reyes <prueba@prueba.com>
2022-11-11 14:00:08 +0100

    Ultimos cambios sql

--- SQL/015Rellenotabla.sql
+++ SQL/015Rellenotabla.sql
@@ -1,20 +1,21 @@
-/*En and se han de cumplir 2 condiciones en caso de or se ha de incluir solo 1
+/*En and se han de cumplir 2 condiciones
+n caso de or se ha de incluir solo 1*/
  
-Insert into Facturas
+/*Insert into Facturas
 values ( 1 ," Ordenador ", " 1000 ");
 Insert into Facturas
 values (2, " Tablet ", " 200 ");
 Insert into Facturas
 values (3, " Raton ", " 50 ");
 Insert into Facturas
-values (4, " Consola ", " 600 ");
+values (4, " Consola ", " 600 ");*/
 
-SELECT * from Facturas
+/*SELECT * from Facturas*/
 
-delete from facturas;
-SELECT * from Facturas
+/*delete from facturas;
+SELECT * from Facturas */
 
-Insert into Facturas
+/*Insert into Facturas
 values ( 1 ," Ordenador ", " 1000 ", "empresaA");
 Insert into Facturas
 values (2, " Tablet ", " 200 ","empresaA");
--- SQL/020DescripcionTabla.sql
+++ SQL/020DescripcionTabla.sql
@@ -3,17 +3,16 @@
 alter table Facturas
  add column empresa VARCHAR(20);
 
-DESCRIBE Facturas 
+DESCRIBE Facturas */
  
-Select * from facturas 
+/*Select * from facturas 
 DATE Facturas set Empresa="Empresa A"
 
 
 insert into facturas 
 values (5,"libro",20,"empresa B");
 insert into facturas 
-values (6,"boligrafo",10,"empresa c"); 
-Select * from facturas;*/ 
+values (6,"boligrafo",10,"empresa c"); */
 
 
 Select * from facturas;
